67373aa3b3a8f4736a5aacb91b0c9472e8c6e2c9
iamcharlesm <because@charlesis.me>
2019-01-08 22:40:53 -0600

    Home and About routes created and rendered, some text added on the About page.

---
+++ src/components/Hello.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Hello',
+  data () {
+    return {
+      msg: 'Welcome to V-Crypt'
+    }
+  }
+}
+</script>
+
+
+<style scoped>
+
+</style>
