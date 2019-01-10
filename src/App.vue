a2034c6dfe286e394e7f6bbb30b9b57f76958a64
iamcharlesm <because@charlesis.me>
2019-01-10 15:35:47 -0600

    Logo added, added api call for Coins.

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <template>
   <div id="app">
+    <img src="./assets/logo.png">
     <router-link :to="{ name: 'Hello' }">Home</router-link>
     <router-link to="/about">About</router-link>
+    <router-link to="/coins/ethereum">Ethereum</router-link>
+    <router-link to="/coins/bitcoin">Bitcoin</router-link>
     <router-view></router-view>
   </div>
 </template>
@@ -20,5 +23,15 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  background-color: #a2aab1;
+  margin: 0 auto;
+}
+
+body {
+  margin: 0 auto;
+}
+
+img {
+  max-height: 300px;
 }
 </style>
--- src/components/Coins.vue
+++ src/components/Coins.vue
@@ -1,15 +1,43 @@
 <template>
-  <div class="coins">
-    <h1>Coins</h1>
-    <p>
-      This is where coins will appear</a>.
-    </p>
+  <div>
+    <p>Name: {{ coin.name }}</p>
+    <p>Symbol: {{ coin.symbol }}</p>
+    <p>Price (USD): {{coin.price_usd }}</p>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
   export default {
-    name: 'Coins'
+    name: 'Coins',
+
+    data() {
+      return {
+        coin: {}
+      }
+    },
+
+    created() {
+      this.fetchData()
+    },
+
+    watch: {
+      '$route': 'fetchData'
+    },
+
+    methods: {
+      fetchData() {
+        axios.get('https://api.coinmarketcap.com/v1/ticker/'+this.$route.params.id+'/')
+        .then((resp) => {
+          this.coin = resp.data[0]
+          console.log(resp)
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+      }
+    },
   }
 </script>
 
